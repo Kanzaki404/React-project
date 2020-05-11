f6373c487c0b0e83f222bf8714c33f7afb568a28
Kanzaki404 <alexander.risteski23@gmail.com>
2020-05-11 20:29:41 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-import './App.scss';
+import './App.scss'
 import Navbar from './components/navBar/navBar'
 import List from './components/list/list'
 
-function App() {
-  //state of cuttent tab selected by user to chose which list to render
+function App () {
+  // state of cuttent tab selected by user to chose which list
   const [currentTab, setCurrentTab] = useState('Search')
 
-  function tab(category){
+  function tab (category) {
     setCurrentTab(category)
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar tabCallback={(cat) => tab(cat) }/>
-          <h1 className="titleTab">{currentTab}</h1>
+    <div className='App'>
+      <header className='App-header'>
+        <Navbar tabCallback={(cat) => tab(cat)} />
+        <h1 className='titleTab'>{currentTab}</h1>
       </header>
       <main>
-      <div className="content">
-          <List tabSelected={currentTab}/>
-      </div>
+        <div className='content'>
+          <List tabSelected={currentTab} />
+        </div>
       </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
